6faaf3a92d70fe3c18240d0b107df767b4a33922
Katlego Komane <113278179+CynicsTech@users.noreply.github.com>
2023-01-08 13:49:40 +0200

    698

--- script.js
+++ script.js
@@ -3,5 +3,8 @@ window.addEventListener('loas', ()=> {
     const form = document.querySelector('form');
     const input = document.querySelector('task_input');
     const list_tas = document.querySelector('tasks');
-
+    
+    form.addEventListener('submit', (e) => {
+        
+    })
 })
